dec95c2860204b5bb56aa10c02b7d08c82280708
K1zunaAiBOT <101422099+K1zunaAiBOT@users.noreply.github.com>
2022-05-29 00:29:02 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -37,3 +37,17 @@ async def on_message(message):
         await message.channel.send(dice)
 
 client.run(TOKEN)
+
+import random
+
+def diceroll(cnt, max):
+    total = 0
+    num_list = []
+    for i in range(0, cnt):
+        # ランダムに1からサイコロの面数までの和を取得しリストに入れる
+        num = random.randint(1, max)
+        num_list.append(num)
+    # さいころの目の総和を計算しリストに入れる
+    total = sum(num_list)
+    num_list.append(total)
+    return num_list
